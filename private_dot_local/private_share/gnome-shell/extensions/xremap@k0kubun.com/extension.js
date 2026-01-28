cd26abcbaa1561ace2e258f1107e7f2a0dabb2e6
tomixrm <chocolate0785lego@icloud.com>
2026-01-29 00:27:44 +0900

    ime autostart

---
+++ private_dot_local/private_share/gnome-shell/extensions/xremap@k0kubun.com/extension.js
@@ -0,0 +1,61 @@
+// Copyright (C) 2022 Takashi Kokubun
+// Licence: GPLv2+
+
+const { Gio } = imports.gi;
+
+class Xremap {
+  constructor() {
+  }
+
+  enable() {
+    const dbus_object = `
+      <node>
+        <interface name="com.k0kubun.Xremap">
+          <method name="ActiveWindow">
+            <arg type="s" direction="out" name="win"/>
+          </method>
+          <method name="WMClass">
+            <arg type="s" direction="out" name="win"/>
+          </method>
+          <method name="WMClasses">
+            <arg type="s" direction="out" name="win"/>
+          </method>
+        </interface>
+      </node>
+    `;
+    this.dbus = Gio.DBusExportedObject.wrapJSObject(dbus_object, this);
+    this.dbus.export(Gio.DBus.session, '/com/k0kubun/Xremap');
+  }
+
+  disable() {
+    this.dbus.flush();
+    this.dbus.unexport();
+    delete this.dbus;
+  }
+
+  ActiveWindow() {
+    const actor = global.get_window_actors().find(a=>a.meta_window.has_focus()===true);
+    if (actor) {
+      const w = actor.get_meta_window();
+      return JSON.stringify({ wm_class: w.get_wm_class(), title: w.get_title() });
+    } else {
+      return '{}';
+    }
+  }
+
+  WMClass() {
+    const actor = global.get_window_actors().find(a=>a.meta_window.has_focus()===true);
+    return actor && actor.get_meta_window().get_wm_class();
+  }
+
+  // To see the application names through the busctl
+  WMClasses() {
+    // Even if it makes the items in a list joined by "\n", dbus output escapes the new line characters.
+    // So this outputs JSON array string instead of the plain text for understandability.
+    return JSON.stringify([...new Set(global.get_window_actors().map(a => a.get_meta_window().get_wm_class()))]);
+  }
+}
+
+function init() {
+  return new Xremap();
+}
